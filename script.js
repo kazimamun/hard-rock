ffbcc61af212629dc117c8f829d3e07646d6477d
kazimamun <41811956+kazimamun@users.noreply.github.com>
2020-08-31 08:43:12 +0600

    script updated

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const searchBtn = document.getElementById('search');
 searchBtn.addEventListener('click',()=>{
     const lyrics = document.getElementById('lyrics');
     getLyrics(lyrics.value);
-    lyrics.value = '';
 })
 
 
@@ -21,10 +20,10 @@ function getLyrics(lyrics){
                                     <p class="author lead">Album by <span>${lyric.artist.name}</span></p>
                                 </div>
                                 <div class="col-md-3 text-md-right text-center">
-                                    <button class="btn btn-success" onClick="getFullLyric('${lyric.artist.name}','${lyric.album.title}')">Get Lyrics</button>
+                                    <button class="btn btn-success" onClick="getFullLyric('${lyric.artist.name}','${lyric.title}')">Get Lyrics</button>
                                 </div>
                             `
-                displayResult.appendChild(div);               
+                displayResult.appendChild(div); 
             });
         });
 };
@@ -32,7 +31,33 @@ function getLyrics(lyrics){
 function getFullLyric(artist,title){
     fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
         .then(res=> res.json())
-        .then(data=>{
-            console.log(data)
+        .then(data=>{            
+            const contentArea = document.querySelector('.content-area');
+            errorData(data, contentArea, title);
         })
 };
+
+function errorData(data, contentArea, title){    
+    const div = document.createElement('div');
+    div.classList = 'single-lyrics text-center';
+    div.innerHTML = `
+                        <button class="btn go-back">&lsaquo;</button>
+                        <h2 class="text-success mb-4">${title}</h2>                            
+                    `
+    const pre = document.createElement('pre');
+    pre.classList = 'lyric text-white';
+    if(data.error){        
+        pre.innerHTML = '<h3>Lyrics Not Found</h3>' ;
+    }else{
+        pre.innerText = data.lyrics;
+    }  
+    const reloadBtn = document.createElement('button');
+    reloadBtn.innerText = 'Search Again';
+    reloadBtn.classList = 'btn btn-success btn-lg btn-block';
+    reloadBtn.addEventListener('click',()=>{
+        location.reload();
+    })
+    // contentArea.innerText = '';
+    div.appendChild(pre);
+    contentArea.append(div,reloadBtn);
+}
