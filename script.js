05e9b70b02fa01d7a2ea263a60bbbec491993231
Kazi Mamun <kazi299499@gmail.com>
2020-08-31 06:31:58 +0600

    final upload

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ function getLyrics(lyrics){
                 div.classList = 'single-result row align-items-center my-3 p-3';
                 div.innerHTML = `
                                 <div class="col-md-9">
-                                    <h3 class="lyrics-name">${lyric.album.title}</h3>
+                                    <h3 class="lyrics-name">${lyric.title}</h3>
                                     <p class="author lead">Album by <span>${lyric.artist.name}</span></p>
                                 </div>
                                 <div class="col-md-3 text-md-right text-center">
-                                    <button class="btn btn-success" onClick="getFullLyric('${lyric.artist.name}','${lyric.album.title}')">Get Lyrics</button>
+                                    <button class="btn btn-success" onClick="getFullLyric('${lyric.artist.name}','${lyric.title}')">Get Lyrics</button>
                                 </div>
                             `
                 displayResult.appendChild(div);               
@@ -34,5 +34,16 @@ function getFullLyric(artist,title){
         .then(res=> res.json())
         .then(data=>{
             console.log(data)
+            const div = document.createElement('div');
+            div.classList = 'single-lyrics text-center';
+            div.innerHTML = `
+                            <button class="btn go-back">&lsaquo;</button>
+                            <h2 class="text-success mb-4">${title}</h2>
+                            <pre class="lyric text-white">
+                                ${data.lyrics}
+                            </pre>
+                        `
+            const contentArea = document.querySelector('.content-area');
+            contentArea.appendChild(div);
         })
 };
